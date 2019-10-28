01bdee6195909b7f81e3c7893511d2f6e2e7fde2
Bagas Pratama <bagas.pratama@wearesocialights.com>
2019-10-29 01:09:40 +0700

    Part 9

--- src/containers/pages/Dashboard/index.js
+++ src/containers/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import './dashboard.scss';
-import { addDataToAPI, getDataFormAPI } from '../../../config/redux/action';
+import { addDataToAPI, getDataFormAPI, updateDataAPI } from '../../../config/redux/action';
 import { connect } from 'react-redux';
 
 
@@ -9,7 +9,9 @@ class Dashboard extends Component{
     state = {
         title: '',
         content: '',
-        date: ''
+        date: '',
+        textButton: 'SIMPAN',
+        noteId: ''
     }
 
     componentDidMount(){
@@ -20,8 +22,8 @@ class Dashboard extends Component{
     }
 
     handleSavedNotes = () => {
-        const {title, content} = this.state;
-        const {saveNotes} = this.props;
+        const {title, content, textButton, noteId } = this.state;
+        const {saveNotes, updateNotes} = this.props;
         const userData = JSON.parse(localStorage.getItem('userData'))
 
         const data = {
@@ -30,9 +32,16 @@ class Dashboard extends Component{
             date: new Date().getTime(),
             userId: userData.uid
         }
-        saveNotes(data)
-        console.log('Simpan', this.props)
-        console.log(data)
+
+        if(textButton === 'SIMPAN'){
+            saveNotes(data)
+        } else{
+            data.noteId = noteId;
+            updateNotes(data)
+        }
+        
+        // console.log('Simpan', this.props)
+        // console.log(data)
     }
 
     onInputChange = (e, type) => {
@@ -41,16 +50,44 @@ class Dashboard extends Component{
         })
     }
 
+    updateNotes = (note) => {
+        console.log(note)
+        this.setState({
+            title: note.data.title,
+            content: note.data.content,
+            textButton: 'UPDATE',
+            noteId: note.id
+        })
+    }
+
+    cancelUpdate = () => {
+        this.setState({
+            title: '',
+            content: '',
+            textButton: 'SIMPAN'
+        })
+    }
+
     render(){
-        const { title, content, date } = this.state;
+        const { title, content, textButton } = this.state;
         const { notes } = this.props;
+        const { updateNotes, cancelUpdate } = this;
         console.log('Notes: ', notes); 
         return(
             <div className="container">
                 <div className="input-form">
                     <input className="input-title" placeholder="title" value={title} onChange={(e) => this.onInputChange(e, 'title')}></input>
                     <textarea className="input-content" placeholder="content" value={content} onChange={(e) => this.onInputChange(e, 'content')}></textarea>
-                    <button className="save-btn" onClick={this.handleSavedNotes}>Simpan</button>
+                    
+                    <div className="action-wrapper">
+                        {
+                            textButton === 'UPDATE' ? (
+                                <button className="save-btn cancel" onClick={cancelUpdate}>CANCEL</button>
+                            ) : <div/> 
+                        }
+                        <button className="save-btn" onClick={this.handleSavedNotes}>{textButton}</button>
+                    </div>
+                    
                 </div>
                 <hr/>
                 {
@@ -59,7 +96,7 @@ class Dashboard extends Component{
                             {
                                 notes.map(note => {
                                     return(
-                                        <div className="card-content" key={note.id}>
+                                        <div className="card-content" key={note.id} onClick={() => updateNotes(note)}>
                                             <p className="title">{note.data.title}</p>
                                             <p className="date">{note.data.date}</p>
                                             <p className="content">{note.data.content}</p>
@@ -83,7 +120,8 @@ const reduxState = (state) => ({
 
 const reduxDispatch = (dispatch) => ({
     saveNotes: (data) => dispatch(addDataToAPI(data)),
-    getNotes: (data) => dispatch(getDataFormAPI(data))
+    getNotes: (data) => dispatch(getDataFormAPI(data)),
+    updateNotes: (data) => dispatch(updateDataAPI(data))
 })
 
 export default connect(reduxState, reduxDispatch) (Dashboard); 
